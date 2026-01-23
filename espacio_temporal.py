71df96383b09b6c01ad61768d2d64c39b3a75b60
Juan D'Adamo <34071165+juandadamo@users.noreply.github.com>
2026-01-23 17:20:56 -0300

    feb26

--- espacio_temporal.py
+++ espacio_temporal.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import savemat
-
+import glob
 
 caso = 'full_freq_13.4'
-A = np.load('data_out/'+caso+'.npz')
 
-Imagen_sum = A['Imagen_sum']
-A_curva_i = A['A_curva_i']
-dictsal = {'Imagen_sum':Imagen_sum,'A_curva_i':A_curva_i}
-xs = np.linspace(0,1280,1280)
-#for Ai in A_curva_i:
-#    plt.plot(xs,Ai,'.')
+lista_casos = np.sort(glob.glob('data_out/'+'*.npz'))
 
-savemat('data_out/'+caso+'.mat',dictsal)
+for caso in lista_casos:
+    A = np.load(caso)
+
+    Imagen_sum = A['Imagen_sum']
+    A_curva_i = A['A_curva_i']
+    dictsal = {'Imagen_sum':Imagen_sum,'A_curva_i':A_curva_i}
+    xs = np.linspace(0,1280,1280)
+    #for Ai in A_curva_i:
+    #    plt.plot(xs,Ai,'.')
+
+    savemat(caso.replace('.npz','.mat'),dictsal)
